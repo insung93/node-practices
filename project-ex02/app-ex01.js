7d0d248c0de571f5ae2d3351206d1a0b182906d0
insung93 <jis5780@gmail.com>
2021-07-09 15:58:33 +0900

    모듈 연습#2

---
+++ project-ex02/app-ex01.js
@@ -0,0 +1,8 @@
+/* 
+    insung93-math 의 js 파일 모듈 테스트
+*/
+const dzMath = require("../insung93-math/insung93");
+
+console.log(dzMath.sum(1,2,3));
+console.log(dzMath.max(1,2,3));
+console.log(dzMath.min(1,2,3));
---
+++ project-ex02/app-ex02.js
@@ -0,0 +1,10 @@
+/*
+    insung93-math npm 모듈 테스트(모듈 패키지 : 로컬 배포)
+    npm i ../insung93-math 명령으로 설치 후, 테스트 할 것
+    
+*/
+const dzMath = require('insung93-math');
+
+console.log(dzMath.sum(1,2,3));
+console.log(dzMath.max(1,2,3));
+console.log(dzMath.min(1,2,3));
