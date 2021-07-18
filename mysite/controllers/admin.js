fb666b0f05733b789cb83a5bdf80cc1f17c5cb59
insung93 <jis5780@gmail.com>
2021-07-18 23:37:07 +0900

    mysite #admin navigation

--- mysite/controllers/admin.js
+++ mysite/controllers/admin.js
@@ -3,10 +3,24 @@ const models = require('../models');
 module.exports = {
     index: async function (req, res) {
         try {
-            const site = await models.Site.findOne({
-                attributes: ['title', 'welcome', 'profile', 'description']
-            });
-            res.render('admin/main', { site });
+            const menu = {"selected":req.params.menu}
+            switch(req.params.menu){
+                case "main":
+                    const site = await models.Site.findOne({
+                        attributes: ['title', 'welcome', 'profile', 'description']
+                    });
+                    res.render('admin/main', { site, menu});
+                    break;
+                case "guestbook":
+                    res.render('admin/guestbook',{menu});
+                    break;
+                case "board":
+                    res.render('admin/board',{menu});
+                    break;
+                case "user":
+                    res.render('admin/user',{menu});
+                    break;
+            } 
         } catch (e) {
             next(e);
         }
--- mysite/routes/admin.js
+++ mysite/routes/admin.js
@@ -3,7 +3,6 @@ const authorized = require('./authorized');
 const controller = require('../controllers/admin');
 
 const router = express.Router();
-router.route('').get(controller.index);
-
+router.route('/:menu').get(controller.index);
 
 module.exports = router;
